044810a574882d9f0d77ce2382e4358e00be5c13
Michael Henderson <mhenderson@keyfactor.com>
2023-07-11 16:31:56 -0700

    batty

--- src/update-topics.js
+++ src/update-topics.js
@@ -26,14 +26,14 @@ async function getRepoTopics(owner, repo) {
 async function updateRepoTopics(owner, repo, names) {
   console.log(`Replacing topic.names with ${names}`);
   try {
-  //  const response = await github.request("PUT /repos/{owner}/{repo}/topics", {
-  //    owner,
-  //    repo,
-  //    names
-  //  });
+    const response = await github.request("PUT /repos/{owner}/{repo}/topics", {
+      owner,
+      repo,
+      names
+    });
     
-    const repoTopics = response.data.names;
-    return repoTopics;
+    //const repoTopics = response.data.names;
+    //return repoTopics;
 
   } catch (e) {
     console.log(e.message)
