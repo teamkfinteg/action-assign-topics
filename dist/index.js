bec0d90efe638dbd97284016c1290268e3a8a706
Michael Henderson <mhenderson@keyfactor.com>
2023-07-11 15:01:24 -0700

    uh

--- dist/index.js
+++ dist/index.js
@@ -16471,7 +16471,7 @@ async function updateTopic(owner, repo, path) {
         if (!repoTopics.includes(t)) {
           repoTopics.push(t);
           console.log(repoTopics);
-          const dbg = JSON.parse(ghcontext)
+          const dbg = JSON.parse(JSON.stringify(ghcontext))
           const dbgb = JSON.parse(github)
           // console.log(`Contents of github: ${dbgb}`)
           console.log(`Contents of ghcontext: ${dbg}`)
