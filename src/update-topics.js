29574db18ee1750f985624ea9ed9244625e60272
Michael Henderson <mhenderson@keyfactor.com>
2023-07-11 16:29:39 -0700

    isolate

--- src/update-topics.js
+++ src/update-topics.js
@@ -25,12 +25,12 @@ async function getRepoTopics(owner, repo) {
 }
 async function updateRepoTopics(owner, repo, names) {
   console.log(`Replacing topic.names with ${names}`);
-  try {
-    const response = await github.request("PUT /repos/{owner}/{repo}/topics", {
-      owner,
-      repo,
-      names
-    });
+  //try {
+  //  const response = await github.request("PUT /repos/{owner}/{repo}/topics", {
+  //    owner,
+  //    repo,
+  //    names
+  //  });
     
     const repoTopics = response.data.names;
     return repoTopics;
