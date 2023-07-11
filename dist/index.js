3f91b3c4a48563b6ae1d0c607cd364f20355432c
Michael Henderson <mhenderson@keyfactor.com>
2023-07-11 15:06:31 -0700

    b

--- dist/index.js
+++ dist/index.js
@@ -16471,7 +16471,7 @@ async function updateTopic(owner, repo, path) {
         if (!repoTopics.includes(t)) {
           repoTopics.push(t);
           console.log(repoTopics);
-          console.log(`Contents of ghcontext: ${JSON.parse(ghcontext)}`)
+          console.log(`Contents of ghcontext: ${JSON.stringify(ghcontext)}`)
           const response = ghcontext.rest.repos.replaceAllTopics({
             owner,
             repo,
